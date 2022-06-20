6f4c5ab437385931445f85ceb7ccac22ea546162
Segun JO <profjaymax@gmail.com>
2022-06-20 13:33:56 +0100

    Django Blog
    Django Blog App

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Published")
+)
+
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now= True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
